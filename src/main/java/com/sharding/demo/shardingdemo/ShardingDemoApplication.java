f060ead1c04ffb4f6cce909d62477f3f33a7060f
star362 <star362@163.com>
2022-04-16 11:29:31 +0800

    提交

---
+++ src/main/java/com/sharding/demo/shardingdemo/ShardingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sharding.demo.shardingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sharding/demo/shardingdemo/ShardingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharding.demo.shardingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
