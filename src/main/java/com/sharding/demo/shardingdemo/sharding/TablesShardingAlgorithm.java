b8630a378e29c8dd11cb8d7105ec08d58c980956
star362 <star362@163.com>
2022-04-19 22:14:59 +0800

    复合分片算法实现!

--- src/main/java/com/sharding/demo/shardingdemo/sharding/TablesShardingAlgorithm.java
+++ src/main/java/com/sharding/demo/shardingdemo/sharding/TablesShardingAlgorithm.java
@@ -1,61 +1,67 @@
 package com.sharding.demo.shardingdemo.sharding;
 
-import org.apache.shardingsphere.sharding.api.sharding.standard.PreciseShardingValue;
-import org.apache.shardingsphere.sharding.api.sharding.standard.RangeShardingValue;
-import org.apache.shardingsphere.sharding.api.sharding.standard.StandardShardingAlgorithm;
+import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingAlgorithm;
+import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.Properties;
 
 /**
- * Description: 分表策略<br>
+ * Description: 分表策略 复合分片策略<br>
  * Create Date: 2022/4/16 下午1:50 <br>
  *
  * @author wangyu@mvtech.com.cn
  */
-//@Component
-public class TablesShardingAlgorithm implements StandardShardingAlgorithm<Date> {
+public class TablesShardingAlgorithm implements ComplexKeysShardingAlgorithm<Date> {
 
     private static final Logger log = LoggerFactory.getLogger(TablesShardingAlgorithm.class);
 
+    private Properties props = new Properties();
+
+    /**
+     * @param collection               表集合
+     * @param complexKeysShardingValue 对象
+     * @return
+     */
     @Override
-    public String doSharding(Collection<String> collection, PreciseShardingValue<Date> preciseShardingValue) {
-
-        log.info("==========");
-        throw new RuntimeException("数据库路由错误");
-    }
-
-    @Override
-    public Collection<String> doSharding(Collection<String> collection, RangeShardingValue<Date> rangeShardingValue) {
-
-
-        log.info("==2222========");
-        throw new RuntimeException("数据库路由错误");
+    public Collection<String> doSharding(Collection<String> collection, ComplexKeysShardingValue<Date> complexKeysShardingValue) {
+        System.out.println();
+//        collection.forEach(S -> {
+//            log.info("=============[{}]", S);
+//        });
+        //对象
+        final Map<String, Collection<Date>> valuesMap = complexKeysShardingValue.getColumnNameAndShardingValuesMap();
+        valuesMap.forEach((k, v) -> {
+            log.info("====k:[{}]========v=[{}]", k, v);
+        });
+//        log.info("=============[{}]", valuesMap);
+        Collection<String> result = new LinkedList();
+        result.add("test_2021_2");
+        return result;
     }
 
     @Override
     public void init() {
-        log.info("=init=2222========");
+
     }
 
     @Override
     public String getType() {
-        log.info("==44========");
-        return null;
+        return "COMP_TEST";
     }
 
     @Override
     public Properties getProps() {
-        log.info("==33========");
-        return null;
+        return this.props;
     }
 
     @Override
-    public void setProps(Properties properties) {
-        log.info("==11========");
+    public void setProps(Properties props) {
+        this.props = props;
     }
 }
