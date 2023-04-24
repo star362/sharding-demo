fb30c5d8c434f0b52620463971807b27d7b72fff
star362 <star362@163.com>
2023-04-24 13:11:49 +0800

    1

--- src/main/java/com/sharding/demo/shardingdemo/coltroller/TestController.java
+++ src/main/java/com/sharding/demo/shardingdemo/coltroller/TestController.java
@@ -1,11 +1,15 @@
 package com.sharding.demo.shardingdemo.coltroller;
 
+import cn.hutool.core.collection.ListUtil;
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
 import com.sharding.demo.shardingdemo.entity.StarDemoEntity;
 import com.sharding.demo.shardingdemo.entity.TestEntity;
 import com.sharding.demo.shardingdemo.service.StarDemoService;
 import com.sharding.demo.shardingdemo.service.TestService;
 import com.sharding.demo.shardingdemo.service.impl.StarDemoServiceImpl;
 import com.sharding.demo.shardingdemo.service.impl.TestSreviceImpl;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.infra.hint.HintManager;
 import org.apache.shardingsphere.transaction.annotation.ShardingSphereTransactionType;
@@ -16,6 +20,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -71,6 +78,31 @@ public class TestController {
 
 
         return "success";
+    }
+
+
+    @SneakyThrows
+    @GetMapping("exportprodsku")
+    public void exportprodsku(
+            HttpServletResponse response) {
+
+
+        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setCharacterEncoding("utf-8");
+        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+        String fileName = URLEncoder.encode("测试", "UTF-8").replaceAll("\\+", "%20");
+        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
+
+        ExcelWriter build = EasyExcel.write(response.getOutputStream()).autoCloseStream(false).build();
+        for (int i = 0; i <= 2000; i++) {
+            build.write(ListUtil.of("对象集合"), EasyExcel.writerSheet("模板").build());
+        }
+
+        build.close();
+
+
+        log.info("over===={}", LocalDateTime.now());
     }
 
 
